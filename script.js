b77be29881b7eae28888a0294f86c43f61ba77f8
Amr Eid <amr_fci@outlook.com>
2023-09-20 10:27:49 +0300

    ...

--- script.js
+++ script.js
@@ -1,38 +1,68 @@
+// var apiUrl = "https://api.mottasl.com/v2";
+// var apiKey = "test-key";
+
+// function tryApi() {
+//     getData(apiUrl + '/customer').then((data) => {
+//         console.log(data);
+//     });
+
+//     // $.ajax({
+//     //     url: apiUrl + '/account/templates',
+//     //     contentType: 'application/json',
+//     //     // dataType: 'json',
+//     //     type: 'GET',
+//     //     headers: {
+//     //         apikey: apiKey,
+//     //         'Access-Control-Allow-Origin': '*'
+//     //     },
+//     //     success: function (data) {
+//     //         console.log(data);
+//     //     }, error: function (xhr, ajaxOptions, thrownError) {
+//     //         console.log(xhr);
+//     //         console.log(ajaxOptions);
+//     //         console.log(thrownError);
+//     //     }
+//     // });
+// }
+
+// async function getData(url = "") {
+//     const response = await fetch(url, {
+//         method: "GET",
+//         mode: "no-cors", // no-cors, *cors, same-origin
+//         headers: {
+//             "Content-Type": "application/json",
+//             "apikey": apiKey,
+//         },
+//     });
+//     return response.json();
+// }
+
 var apiUrl = "https://api.mottasl.com/v2";
 var apiKey = "test-key";
 
-function tryApi() {
-    getData(apiUrl + '/account/templates').then((data) => {
+async function tryApi() {
+    try {
+        const data = await getData(apiUrl + '/customer');
         console.log(data);
-    });
-
-    // $.ajax({
-    //     url: apiUrl + '/account/templates',
-    //     contentType: 'application/json',
-    //     // dataType: 'json',
-    //     type: 'GET',
-    //     headers: {
-    //         apikey: apiKey,
-    //         'Access-Control-Allow-Origin': '*'
-    //     },
-    //     success: function (data) {
-    //         console.log(data);
-    //     }, error: function (xhr, ajaxOptions, thrownError) {
-    //         console.log(xhr);
-    //         console.log(ajaxOptions);
-    //         console.log(thrownError);
-    //     }
-    // });
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
 
 async function getData(url = "") {
     const response = await fetch(url, {
         method: "GET",
-        mode: "no-cors", // no-cors, *cors, same-origin
         headers: {
             "Content-Type": "application/json",
             "apikey": apiKey,
         },
     });
+
+    if (!response.ok) {
+        throw new Error(`Request failed with status ${response.status}`);
+    }
+
     return response.json();
 }
+
+// tryApi();
